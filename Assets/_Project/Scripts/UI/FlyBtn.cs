7589ee9006485fcb13e85a8d590b0c185af08a9f
NamLh <namlh@gmaeestudio.com>
2023-04-26 11:43:05 +0700

    Add Logic Update Count Text Fly and Switch Tool

--- Assets/_Project/Scripts/UI/FlyBtn.cs
+++ Assets/_Project/Scripts/UI/FlyBtn.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using Pancake;
+using TMPro;
 using UnityEngine;
 
 public class FlyBtn : MonoBehaviour
 {
     private bool _isFlying;
+    [SerializeField] private TextMeshProUGUI textCount;
+    private void Start()
+    {
+        Initialize();
+    }
+    void Initialize()
+    {
+        UpdateTextCount();
+    }
+    void UpdateTextCount()
+    {
+        textCount.text = Data.FlyToolCount.ToString();
+    }
     private void OnEnable()
     {
         Observer.OnSwapping += Swapping;
         Observer.EndSwapping += EndSwapping;
+        Observer.CountFly += UpdateTextCount;
     }
     private void OnDisable()
     {
         Observer.OnSwapping -= Swapping;
         Observer.EndSwapping -= EndSwapping;
+        Observer.CountFly -= UpdateTextCount;
     }
     public void DoFly()
     {
-        if (!_isFlying)
+        if (!_isFlying && Data.FlyToolCount > 0)
         {
             Observer.OnSwapping?.Invoke();
             PopupController.Instance.Show<PopupFlyTool>();
--- Assets/_Project/Scripts/UI/SwitchBtn.cs
+++ Assets/_Project/Scripts/UI/SwitchBtn.cs
@@ -1,24 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class SwitchBtn : MonoBehaviour
 {
     private bool _isSwapping;
+    [SerializeField] private TextMeshProUGUI textCount;
+    private void Start()
+    {
+        Initialize();
+    }
+    void Initialize()
+    {
+        UpdateTextCount();
+    }
+    void UpdateTextCount()
+    {
+        textCount.text = Data.SwapToolCount.ToString();
+    }
     private void OnEnable()
     {
         Observer.OnSwapping += Swapping;
         Observer.EndSwapping += EndSwapping;
+        Observer.CountSwap += UpdateTextCount;
     }
     private void OnDisable()
     {
         Observer.OnSwapping -= Swapping;
         Observer.EndSwapping -= EndSwapping;
+        Observer.CountSwap -= UpdateTextCount;
     }
     public void SwitchPosi()
     {
-        if (!_isSwapping)
+        if (!_isSwapping && Data.SwapToolCount > 0)
         {
             Observer.OnSwapping?.Invoke();
             PopupController.Instance.Show<PopupSwapTool>();
