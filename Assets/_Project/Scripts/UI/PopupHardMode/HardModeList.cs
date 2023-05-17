7142021b5e530beb0e22a8b8bf6022fe084c2913
NamLh <namlh@gameestudio.com>
2023-05-17 11:33:01 +0700

    Update HardModeList.cs

--- Assets/_Project/Scripts/UI/PopupHardMode/HardModeList.cs
+++ Assets/_Project/Scripts/UI/PopupHardMode/HardModeList.cs
@@ -1,10 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HardModeList : ScrollBoard
 {
-    public override void SetIndexText()
+    [SerializeField] private StateModeData stateModeData;
+    public void SetStateMode(int levelAvailable)
+    {
+        for (int i = 0; i < content.transform.childCount - 2; i++)
+        {
+            var setup = content.transform.GetChild(i).GetComponent<HardModeUI>();
+            if (i < levelAvailable)
+            {
+                if (stateModeData.setStateModes.Count != 0)
+                {
+                    if (i < stateModeData.setStateModes.Count)
+                    {
+                        var getState = stateModeData.setStateModes[i].eStateMode;
+                        setup.ShowState(getState);
+                    }
+                    else
+                    {
+                        setup.ShowState(EStateMode.UnlockItem);
+                    }
+                }
+                else
+                {
+                    setup.ShowState(EStateMode.UnlockItem);
+                }
+            }
+            else
+            {
+                setup.ShowState(EStateMode.Lock);
+            }
+        }
+    }
+    protected override void SetIndexText()
     {
         getObj.GetComponent<HardModeUI>().SetIndexHardMode(index);
     }
