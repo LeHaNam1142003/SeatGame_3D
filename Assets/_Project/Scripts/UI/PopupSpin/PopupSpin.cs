7051fe7e3286cf740ece78454572bc1f5d7dbeb9
NamLh <namlh@gameestudio.com>
2023-05-19 10:39:11 +0700

    Update PopupSpin.cs

--- Assets/_Project/Scripts/UI/PopupSpin/PopupSpin.cs
+++ Assets/_Project/Scripts/UI/PopupSpin/PopupSpin.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PopupSpin : Popup
 {
-    
+    [SerializeField] private TextMeshProUGUI coinText;
+    [SerializeField] private TextMeshProUGUI spinAmountText;
+    protected override void BeforeShow()
+    {
+        coinText.text = Data.CurrencyTotal.ToString();
+        spinAmountText.text = Data.SpinTicketAmount.ToString();
+        base.BeforeShow();
+    }
+    public void DoSpinWithTicket()
+    {
+        if (Data.IsTesting)
+        {
+            Observer.DoSpin?.Invoke();
+        }
+        else
+        {
+            if (Data.SpinTicketAmount <= 0) return;
+            Observer.DoSpin?.Invoke();
+        }
+    }
+    protected override void AfterHidden()
+    {
+        PopupController.Instance.Show<PopupUI>();
+        base.AfterHidden();
+    }
+    public void DoSpinWithWatchAds()
+    {
+
+    }
 }
