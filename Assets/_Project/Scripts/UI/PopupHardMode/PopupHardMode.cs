9e4f40fd1c53dd0e39208ad8a3ebad80169d4a6c
NamLh <namlh@gameestudio.com>
2023-05-30 11:42:26 +0700

    Update PopupHardMode.cs

--- Assets/_Project/Scripts/UI/PopupHardMode/PopupHardMode.cs
+++ Assets/_Project/Scripts/UI/PopupHardMode/PopupHardMode.cs
@@ -1,10 +1,16 @@
 using System;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupHardMode : Popup
 {
     [SerializeField] private HardModeList hardModeList;
+    [SerializeField] private Image buttonHardModeOn;
+    [SerializeField] private Image buttonHardModeOff;
+    [SerializeField] private Image buttonSuperHardModeOn;
+    [SerializeField] private Image buttonSuperHardModeOff;
+    private bool _isSuperHardMode;
     private void Awake()
     {
         Initialization();
@@ -15,7 +21,14 @@ public class PopupHardMode : Popup
     }
     private void OnEnable()
     {
-        hardModeList.SetStateMode(Data.HardModeUnlock);
+        if (_isSuperHardMode)
+        {
+            hardModeList.SetStateMode(Data.SuperHardModeUnlock);
+        }
+        else
+        {
+            hardModeList.SetStateMode(Data.HardModeUnlock);
+        }
         Observer.PlayHardMode += OnPLayHardMode;
     }
     private void OnDisable()
@@ -25,6 +38,39 @@ public class PopupHardMode : Popup
     void OnPLayHardMode(int index)
     {
         GameManager.Instance.StartHardModeGame(index);
+    }
+    void OnPlaySuperHardMode()
+    {
+
+    }
+    public void OnclickSuperHardMode()
+    {
+        setHardModeOn(false);
+        if (Data.HardModeUnlock == 25 && Data.SuperHardModeUnlock == 0)
+        {
+            Data.SuperHardModeUnlock = 1;
+        }
+        SetUp(12, Data.SuperHardModeUnlock);
+    }
+    public void OnClickHardMode()
+    {
+        setHardModeOn(true);
+        SetUp(25, Data.HardModeUnlock);
+    }
+    void setHardModeOn(bool isHardMode)
+    {
+        buttonHardModeOn.gameObject.SetActive(isHardMode);
+        buttonHardModeOff.gameObject.SetActive(!isHardMode);
+        buttonSuperHardModeOn.gameObject.SetActive(!isHardMode);
+        buttonSuperHardModeOff.gameObject.SetActive(isHardMode);
+    }
+    void SetUp(int elements, int CountUnLock)
+    {
+        hardModeList.contentUIs.Clear();
+        hardModeList.Clear();
+        hardModeList.elements = elements;
+        hardModeList.ShowContent();
+        hardModeList.SetStateMode(CountUnLock);
     }
 
 }
