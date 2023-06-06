8f6cea1651c3a4072a4597888eefd75a5d1a053e
NamLh <namlh@gameestudio.com>
2023-06-06 13:57:21 +0700

    Add TrackingMissionBtn.cs

---
+++ Assets/_Project/Scripts/UI/TrackingMissionBtn.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingMissionBtn : MonoBehaviour
+{
+    [SerializeField] private GameObject notice;
+    private void OnEnable()
+    {
+        Observer.ShowNoticeIcon += ShowNoticeIcon;
+    }
+    public void ShowTrackingMission()
+    {
+        PopupController.Instance.Show<PopupTrackingMission>();
+    }
+    void ShowNoticeIcon(bool isShowNotice)
+    {
+        if (notice!=null)
+        {
+             notice.gameObject.SetActive(isShowNotice);
+        }
+    }
+
+}
