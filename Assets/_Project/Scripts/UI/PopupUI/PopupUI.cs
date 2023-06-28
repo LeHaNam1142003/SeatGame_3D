e500b738ee3f4e07212fdedadac35381a9d351cb
NamLh <namlh@gameestudio.com>
2023-06-28 09:58:35 +0700

    Update PopupUI.cs

--- Assets/_Project/Scripts/UI/PopupUI/PopupUI.cs
+++ Assets/_Project/Scripts/UI/PopupUI/PopupUI.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopupUI : Popup
+public class PopupUI : Popup 
 {
+    public bool isShowTicket { get; set; }
+    [SerializeField] private GameObject ticketIcon;
+    protected override void BeforeShow()
+    {
+        ticketIcon.gameObject.SetActive(isShowTicket);
+        base.BeforeShow();
+    }
     public void Debugging()
     {
         Data.CurrencyTotal += 100;
