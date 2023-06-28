683e2258278337f9863bcdf8d9d5357f12b051d9
NamLh <namlh@gameestudio.com>
2023-06-28 09:57:33 +0700

    Update CoinTotal.cs

--- Assets/_Project/Scripts/Common/CoinTotal.cs
+++ Assets/_Project/Scripts/Common/CoinTotal.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class CoinTotal : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI currencyTotal;
-
-    private void Awake()
-    {
-        Observer.CurrencyTotalChanged += UpdateCurrencyText;
-        UpdateCurrencyText();
-    }
-
-    private void UpdateCurrencyText()
-    {
-        currencyTotal.text = Data.CurrencyTotal.ToString();
-    }
+    // [SerializeField] private TextMeshProUGUI currencyTotal;
+    //
+    // private void Awake()
+    // {
+    //     Observer.CurrencyTotalChanged += UpdateCurrencyText;
+    //     UpdateCurrencyText();
+    // }
+    //
+    // private void UpdateCurrencyText()
+    // {
+    //     currencyTotal.text = Data.CurrencyTotal.ToString();
+    // }
 }
