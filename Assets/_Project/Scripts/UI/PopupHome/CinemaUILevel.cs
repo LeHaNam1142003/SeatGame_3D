8c781967599a0a0c179975d0edae14343b6898be
NamLh <namlh@gameestudio.com>
2023-07-01 19:34:43 +0700

    Update CinemaUILevel.cs

--- Assets/_Project/Scripts/UI/PopupHome/CinemaUILevel.cs
+++ Assets/_Project/Scripts/UI/PopupHome/CinemaUILevel.cs
@@ -8,11 +8,25 @@ using UnityEngine.UI;
 
 public class CinemaUILevel : MonoBehaviour
 {
+    [SerializeField] private Sprite normalIcon;
+    [SerializeField] private Sprite specialIcon;
+    [SerializeField] private Image icon;
     [SerializeField] private TextMeshProUGUI levelText;
     [SerializeField] private Image hightlightSelected;
     public void SetLevelText(int indexLevel)
     {
         levelText.text = $"Level {indexLevel}";
+    }
+    public void SetNormal(bool isNormal)
+    {
+        if (isNormal)
+        {
+            icon.sprite = normalIcon;
+        }
+        else
+        {
+            icon.sprite = specialIcon;
+        }
     }
     public void SetHightLight(bool isActive)
     {
